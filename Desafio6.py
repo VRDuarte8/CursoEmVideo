d0b1e50923d924dc9406e48f3153eb6cf24861d7
Vinicius <viniciusrduarte8@gmail.com>
2022-12-26 14:48:52 -0300

    initial commit

---
+++ Desafio6.py
@@ -0,0 +1,2 @@
+n = (int(input("Digite um número: ")))
+print("Dobro: {}\nTriplo: {}\nRaíz: {:.0f}".format(n * 2, n * 3, n ** 0.5))
---
+++ aula4.3.py
@@ -0,0 +1,3 @@
+N1 = input('Primeiro número = ')
+N2 = input('Segundo número = ')
+print('A soma é ',N1+N2)
